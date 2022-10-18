2587822d79e561abd962e7cd5e43049ae2e3138c
Clara Rennesey <84031295+clararennesey@users.noreply.github.com>
2022-10-19 02:57:02 +0700

    Update clararennesey.js

--- clararennesey.js
+++ clararennesey.js
@@ -1,48 +1,73 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const https = require('https');
+const timeEl = document.getElementById("countdown-timer");
+const word = document.getElementById("spellCheck");
+const scoreEl = document.getElementById("scoreBoard");
+//const remains constant throughout, let can be updated
+let time = 11;
+let score = 0;
+scoreEl.innerHTML = score;
+const words = ['sigh',
+'tense',
+'airplane',
+'ball',
+'pies',
+'juice',
+'warlike',
+'bad',
+'north',
+'dependent',
+'steer',
+'silver',
+'highfalutin',
+'superficial',
+'quince',
+'eight',
+'feeble',
+'admit',
+'drag',
+'loving'
+];
 
-const app = express();
+function getRandomElement () {
+    return words[Math.floor(Math.random() * words.length)];
+}
 
-app.set('view engine', 'ejs');
+function addWordToH3 () {
+    document.getElementById("word").innerHTML = getRandomElement();
+}
 
-app.use(bodyParser.urlencoded({extended: true}));
+var countdown = setInterval(updateTime,1000);
 
-app.get("/", (req, res) => {
-   let today = new Date();
-   let options = {
-       weekday: 'long',
-       day: 'numeric',
-       month: 'long',
-       year: 'numeric'
-   };
-    let day = today.toLocaleDateString('en-US', options);
-    res.render("weather", {
-        Today: day,
-    });   
+function updateTime () {
+    time--;
+    timeEl.innerHTML = time;
+
+    if(time == 0)
+    {
+        clearInterval(countdown);
+        document.getElementById("spellCheck").disabled = true;
+        document.getElementById("word").innerHTML = "TIME UP!";
+    }
+}
+
+addWordToH3();
+
+function updateTime2() {
+    time += 2;
+    timeEl.innerHTML = time;
+}
+
+function updateScore () {
+    score += 1;
+    scoreEl.innerHTML = score;
+}
+//the main happening, here we add an event listener with an input to check out for any changes in the input text field. We pass element to the function so as we can target the input text field
+word.addEventListener('input',function(element) {
+    const submission = element.target.value;
+    if(submission === document.getElementById("word").innerHTML) {
+        updateTime2();
+        addWordToH3();
+        // to clear the input area after word entered is correct
+        element.target.value = "";
+        updateScore();
+    }
 });
-
-app.post("/", (req, res) => {
-    const city = req.body.place;
-    const appid = "d8c5d3243355ad76a180185dc2e4935e";
-    const unit = "metric";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=${unit}`;
-    
-    https.get(url, (response) => {    
-         console.log(response.statusCode);
-         response.on("data", (data) => {
-            const WeatherData = JSON.parse(data);
-            const temp =  WeatherData.main.temp;
-            const description = WeatherData.weather[0].description;
-            const icon = WeatherData.weather[0].icon;
-            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
-
-            res.write(`<p> The weather is currently ${description}.</p>`)
-            res.write(`<h1>The temperature in ${city} is ${temp} degrees celcius.</h1>`);
-            res.write(`<img src= ${imageURL} alt="">`)
-            res.send();
-        });
-    });
-});
-
-app.listen(3000, () => console.log('Starting the Weather App'));
